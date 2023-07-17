f4e52d69f3b1e32543a53a652e58de978b364dcf
Xolile Dlamini <xolile@lintam.com>
2023-07-17 20:00:53 +0200

    pokemons application

---
+++ src/app/components/list/list.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { concat, Subscription } from 'rxjs';
+import { PokemonService } from 'src/app/services/pokemon.service';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss']
+})
+export class ListComponent implements OnInit, OnDestroy {
+
+  loading: boolean = false;
+
+  subscriptions: Subscription[] = [];
+
+  constructor(private pokemonService: PokemonService) { }
+
+  get pokemons(): any[] {
+    return this.pokemonService.pokemons;
+  }
+
+  set subscription(subscription: Subscription) {
+    this.subscriptions.push(subscription);
+  }
+
+  ngOnInit(): void {
+    if (!this.pokemons.length) {
+      this.loadMore();
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(subscription => subscription ? subscription.unsubscribe() : 0);
+  }
+
+  loadMore(): void {
+    this.loading = true;
+    this.subscription = this.pokemonService.getNext().subscribe((response: { next: string; results: any[]; }) => {
+      this.pokemonService.next = response.next;
+      const details = response.results.map((i: any) => this.pokemonService.get(i.name));
+      this.subscription = concat(...details).subscribe((response: any) => {
+        this.pokemonService.pokemons.push(response);
+      });
+    }, (error: any) => console.log('Error Occurred:', error), () => this.loading = false);
+  }
+  //type
+  getType(pokemon: any): string {
+    return this.pokemonService.getType(pokemon);
+  }
+
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://pokeapi.co/api/v2/',
+  };
+  
